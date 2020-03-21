6d94d47487d20d2070984a00d82499cf8a6859ed
btate712 <btate712@gmail.com>
2020-03-20 19:36:56 -0500

    Adjusted textarea witdth to accomodate phone screens

--- src/components/ColemanLiauCalculator.js
+++ src/components/ColemanLiauCalculator.js
@@ -26,7 +26,7 @@ class ColemanLiauCalculator extends React.Component {
       <>
         <h1>Coleman-Liau Index Calculator</h1>
         <h3>Please enter the passage you'd like to evaluate...</h3>
-        <textarea cols="80" rows="10" name="input" value={this.state.input} onChange={this.inputChanged} />
+        <textarea cols="55" rows="10" name="input" value={this.state.input} onChange={this.inputChanged} />
         <br />
         <button onClick={this.calculate} >Calculate Coleman-Liau Index</button>
         {this.state.buttonPressed ? <h3>Index: {colemanLiauIndex(this.state.input)}</h3> : <></>}
